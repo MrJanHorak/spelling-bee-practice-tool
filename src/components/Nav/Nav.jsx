fc09044a4163df47eb89b218cf28f62809035a4a
Jan Horak <janny_b_goode@hotmail.com>
2022-02-09 21:22:17 -0500

    add logo to Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import LogoDesktop from '../../assets/logo/logo-desktop.png'
+
 const Nav = (props) => {
   return (
     <nav className="nav-bar">
-      <NavLink id="logo" to="/index" <img src={LogoDesktop} alt="bee" />
+      <NavLink id="logo" to="/index" ><img src={LogoDesktop} alt="bee"/></NavLink>
       {props.user
       ?
     <>
