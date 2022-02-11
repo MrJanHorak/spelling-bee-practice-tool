5f1b231ed6fd9e58f15adc932632a79930faf3a2
Jan Horak <janny_b_goode@hotmail.com>
2022-02-10 20:30:45 -0500

    add basic css to nav bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../../styles/Nav.css";
 
-import LogoDesktop from '../../assets/logo/logo-desktop.png'
+import LogoDesktop from "../../assets/logo/logo-desktop.png";
 
 const Nav = (props) => {
   return (
     <nav className="nav-bar">
-      <NavLink id="logo" to="/index" ><img src={LogoDesktop} alt="bee"/></NavLink>
-      {props.user
-      ?
-    <>
-      <NavLink to="/practice">Spelling Bee Mode</NavLink>
-      <NavLink to="/study">Study Words</NavLink>
-      <NavLink to="/" onClick={props.handleLogout}>Logout</NavLink>
-    </>
-    : <>
-      <NavLink to="/signup">Sign Up</NavLink>
-      <NavLink to="/signin">Sign In</NavLink>
-    </>
-    }
+      <NavLink id="logo" to="/index">
+        <img src={LogoDesktop} alt="bee" />
+      </NavLink>
+      {props.user ? (
+        <>
+          <NavLink to="/practice">Spelling Bee Mode</NavLink>
+          <NavLink to="/study">Study Words</NavLink>
+          <NavLink to="/" onClick={props.handleLogout}>
+            Logout
+          </NavLink>
+        </>
+      ) : (
+        <>
+          <NavLink to="/signup">Sign Up</NavLink>
+          <NavLink to="/signin">Sign In</NavLink>
+        </>
+      )}
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
