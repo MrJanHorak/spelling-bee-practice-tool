f36830609bc6c108d3b1064b225e112d6510035a
Jan Horak <janny_b_goode@hotmail.com>
2022-02-14 22:37:32 -0500

    add apiCall to WordsAPI on Admin page

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import WordSearch from '../../components/WordSearch/WordSearch';
 
 const Admin = (props) => {
+  const [wordData, setWordData] = useState('');
+  const [word, setWord] = useState('')
+
+  const handleSubmit = word => {
+    console.log('App - makeApiCall - word', word);
+    setWord(word)
+  }
+
+  useEffect(() => {
+    let wordUrl = `https://wordsapiv1.p.rapidapi.com/words/${word}/definitions`
+
+  const makeApiCall = () => {
+    fetch(wordUrl)
+    .then(res => res.json())
+    .then(data => {
+      console.log('wordData', data)
+      setWordData(data)
+    })
+  }
+  makeApiCall()
+}, [word])
 
   return (
-    <>
+    <div className="admin">
+
       <p>Add Word</p>
-      
-    </>
+      <WordSearch handleSubmit={handleSubmit} />
+      {word}
+    
+    </div>
   )
 }
 export default Admin
