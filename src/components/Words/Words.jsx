98bed55a8a5a8c10f65477a9b8084b267afeba72
Jan Horak <janny_b_goode@hotmail.com>
2022-03-18 22:22:00 -0400

    add wordlist filter buttons and css styling

--- src/components/Words/Words.jsx
+++ src/components/Words/Words.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import "../../styles/WordList.css"
 
-function Words({allWords}) {
+function Words({ allWords }) {
 
-  console.log(allWords)
-  let wordList = allWords.map((word , i) =>{
+  const wordList = allWords.map((word, i) => {
     return (
       <div key={i}>
-        <br/>
-      <b><h3>{word.word}</h3></b>
-      <b>Part of Speech: </b>{word.partOfSpeech}<br/>
-      <b>Definition: </b>{word.definition}<br/>
-      </div>)
-  })
+        <br />
+        <b>
+          <h3>{word.word}</h3>
+        </b>
+        <b>Part of Speech: </b>
+        {word.partOfSpeech}
+        <br />
+        <b>Definition: </b>
+        {word.definition}
+        <br />
+      </div>
+    );
+  });
 
   return (
     <>
@@ -19,9 +26,18 @@ function Words({allWords}) {
       <h2>Word Lists:</h2>
 
       <form className="register-form">
-        <label htmlFor="word">All stored words:</label>
+<div id="filter-button-container">
+        <input type="button" className="filter-button"  value="1st"/>
+        <input type="button" className="filter-button"  value="2nd"/>
+        <input type="button" className="filter-button"  value="3rd"/>
+        <input type="button" className="filter-button"  value="4th"/>
+        <input type="button" className="filter-button"  value="5th"/>
+        <input type="button" className="filter-button"  value="6th"/>
+        <input type="button" className="filter-button"  value="7th"/>
+        <input type="button" className="filter-button"  value="8th"/>
+        </div>
         <input id="submit-button" type="submit" value="Show All Words" />
-       {wordList}
+        {wordList}
       </form>
     </>
   );
