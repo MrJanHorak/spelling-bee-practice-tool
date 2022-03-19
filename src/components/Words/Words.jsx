f8f05e5ed72235457117003aef3c0d3d5e137116
Jan Horak <janny_b_goode@hotmail.com>
2022-03-18 23:15:58 -0400

    add simple filter functionality to show words

--- src/components/Words/Words.jsx
+++ src/components/Words/Words.jsx
@@ -1,23 +1,47 @@
-import React from "react";
-import "../../styles/WordList.css"
+import React, { useState } from "react";
+import "../../styles/WordList.css";
 
 function Words({ allWords }) {
+  const [filter, setFilter] = useState();
+
+  const handleClick = (e) => {
+    setFilter(e.target.value[0]);
+  };
 
   const wordList = allWords.map((word, i) => {
-    return (
-      <div key={i}>
-        <br />
-        <b>
-          <h3>{word.word}</h3>
-        </b>
-        <b>Part of Speech: </b>
-        {word.partOfSpeech}
-        <br />
-        <b>Definition: </b>
-        {word.definition}
-        <br />
-      </div>
-    );
+    if (parseInt(filter) === word.gradeLevel) {
+      return (
+        <div key={i}>
+          <br />
+          <b>
+            <h3>{word.word}</h3>
+          </b>
+          <b>Part of Speech: </b>
+          {word.partOfSpeech}
+          <br />
+          <b>Definition: </b>
+          {word.definition}
+          <br />
+        </div>
+      );
+    } else if (filter === "S") {
+      return (
+        <div key={i}>
+          <br />
+          <b>
+            <h3>{word.word}</h3>
+          </b>
+          <b>Part of Speech: </b>
+          {word.partOfSpeech}
+          <br />
+          <b>Definition: </b>
+          {word.definition}
+          <br />
+        </div>
+      );
+    } else {
+      return "";
+    }
   });
 
   return (
@@ -26,17 +50,62 @@ function Words({ allWords }) {
       <h2>Word Lists:</h2>
 
       <form className="register-form">
-<div id="filter-button-container">
-        <input type="button" className="filter-button"  value="1st"/>
-        <input type="button" className="filter-button"  value="2nd"/>
-        <input type="button" className="filter-button"  value="3rd"/>
-        <input type="button" className="filter-button"  value="4th"/>
-        <input type="button" className="filter-button"  value="5th"/>
-        <input type="button" className="filter-button"  value="6th"/>
-        <input type="button" className="filter-button"  value="7th"/>
-        <input type="button" className="filter-button"  value="8th"/>
+        <div id="filter-button-container">
+          <input
+            type="button"
+            className="filter-button"
+            value="1st"
+            onClick={handleClick}
+          />
+          <input
+            type="button"
+            className="filter-button"
+            value="2nd"
+            onClick={handleClick}
+          />
+          <input
+            type="button"
+            className="filter-button"
+            value="3rd"
+            onClick={handleClick}
+          />
+          <input
+            type="button"
+            className="filter-button"
+            value="4th"
+            onClick={handleClick}
+          />
+          <input
+            type="button"
+            className="filter-button"
+            value="5th"
+            onClick={handleClick}
+          />
+          <input
+            type="button"
+            className="filter-button"
+            value="6th"
+            onClick={handleClick}
+          />
+          <input
+            type="button"
+            className="filter-button"
+            value="7th"
+            onClick={handleClick}
+          />
+          <input
+            type="button"
+            className="filter-button"
+            value="8th"
+            onClick={handleClick}
+          />
         </div>
-        <input id="submit-button" type="submit" value="Show All Words" />
+        <input
+          id="submit-button"
+          type="button"
+          value="Show All Words"
+          onClick={handleClick}
+        />
         {wordList}
       </form>
     </>
