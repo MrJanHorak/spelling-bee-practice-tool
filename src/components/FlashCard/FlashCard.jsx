d1a863ea5a26493aff87804696953d8c13768b8f
Jan Horak <janny_b_goode@hotmail.com>
2022-03-24 23:53:59 -0400

    add loading message to study page

--- src/components/FlashCard/FlashCard.jsx
+++ src/components/FlashCard/FlashCard.jsx
@@ -2,29 +2,31 @@ import React from "react";
 import "../../styles/FlashCard.css";
 
 const FlashCard = ({ handleClick, displayWord }) => {
-  return (
-    <>
-      <div className="form-container">
-        <div className="flashcard-definition">
-          <div id="word">
-            <h1> {displayWord.word} </h1>
-          </div>
-          <div id="definition">
-            <b>Part of speech: </b> {displayWord.partOfSpeech}
-            <br />
-            <br />
-            <b>Definition: </b>
-            {displayWord.definition}
-          </div>
-          <input
-            id="next-button"
-            type="button"
-            value="Next"
-            onClick={handleClick}
-          />
+  return displayWord.word ? (
+    <div className="form-container">
+      <div className="flashcard-definition">
+        <div id="word">
+          <h1> {displayWord.word} </h1>
         </div>
+        <div id="definition">
+          <b>Part of speech: </b> {displayWord.partOfSpeech}
+          <br />
+          <br />
+          <b>Definition: </b>
+          {displayWord.definition}
+        </div>
+        <input
+          id="next-button"
+          type="button"
+          value="Next"
+          onClick={handleClick}
+        />
       </div>
-    </>
+    </div>
+  ) : (
+    <div>
+      <h2>Loading Flashcards ...</h2>
+    </div>
   );
 };
 
