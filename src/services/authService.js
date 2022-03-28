479897fcf7775c9cda6ba02b347c524554e27916
Jan Horak <janny_b_goode@hotmail.com>
2022-03-28 13:56:46 -0400

    refactor authService for CORS error correction

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,11 @@
 import * as tokenService from "./tokenService";
 const BASE_URL = "https://spelling-bee-practice-app.herokuapp.com/api/auth";
 
-//help whats going on?
 async function signup(user) {
   try {
     const res = await fetch(`${BASE_URL}/signup`, {
       method: "POST",
-      headers: new Headers({ "Content-Type": "application/json" }),
+      headers: new Headers({ "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" }),
       body: JSON.stringify(user),
     });
     const json = await res.json();
@@ -34,7 +33,7 @@ async function login(credentials) {
   try {
     const res = await fetch(`${BASE_URL}/login`, {
       method: "POST",
-      headers: new Headers({ "Content-Type": "application/json" }),
+      headers: new Headers({ "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" }),
       body: JSON.stringify(credentials),
     });
     const json = await res.json();
