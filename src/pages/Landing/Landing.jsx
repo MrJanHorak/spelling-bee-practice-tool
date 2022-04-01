9858e3d9375fdd14a165f6e0e962e6054c0d29db
Jan Horak <janny_b_goode@hotmail.com>
2022-04-01 12:27:25 -0400

    tweak styling for landing page and study page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,8 +8,11 @@ const Landing = ({ user }) => {
 
   return (
     <div id="landing-page">
-      <h1>Spelling Bee Practice app!</h1>
+      <h1>Spelling Bee Practice App</h1>
+      <div className='flashcard'>
       <p>Am I really loading?</p>
+
+      </div>
     </div>
   )
 }
