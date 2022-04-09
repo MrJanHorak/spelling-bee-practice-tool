78b762e1fc1f03af7a504eb777694840c7b19552
Jan Horak <janny_b_goode@hotmail.com>
2022-04-08 23:59:16 -0400

    begin writing a function to display letter for letter

--- src/components/FlashCard/FlashCard.jsx
+++ src/components/FlashCard/FlashCard.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSpeechSynthesis } from "react-speech-kit";
-
 
 import "../../styles/FlashCard.css";
 
 const FlashCard = ({ handleClick, displayWord }) => {
+  const [speaking, setSpeaking] = useState(false);
+  const { speak } = useSpeechSynthesis();
 
-    const { speak } = useSpeechSynthesis();
+  let toSpell = [];
+  // let character = useRef();
 
-    const spellWord = (e) => {
-    const toSpell = displayWord.word.split('')
-    toSpell.forEach(letter => speak({text: letter}))
-    console.log(toSpell)
+  const spellWord = (e) => {
+    toSpell = displayWord.word.split("");
+    let time = toSpell.length * 1000 + 1000;
+    setTimeout(() => {
+      setSpeaking(false);
+    }, [time]);
+    setSpeaking(true);
+    toSpell.forEach((letter) => {
+      speak({ text: letter });
+    });
+  };
 
-  }
+  // useEffect(() => {
+  //   let toDisplay = [];
+  //   if (speaking) {
+  //     toDisplay = displayWord.word.split("");
+  //     console.log(speaking);
+  //     console.log(toDisplay);
+  //     character.current = toDisplay.map((letter) => {
+  //       console.log(letter);
+  //       return <span key={letter.charCodeAt()}>{letter}</span>;
+  //     });
+  //   }
+  // });
 
   return displayWord.word ? (
     <div className="form-container">
       <div className="flashcard-definition">
         <div id="word">
-          <h1> {displayWord.word} </h1>
+          {speaking ? <h1> SPELLING </h1> : <h1> {displayWord.word} </h1>}
         </div>
         <div id="definition">
           <b>Part of speech: </b> {displayWord.partOfSpeech}
@@ -29,18 +49,8 @@ const FlashCard = ({ handleClick, displayWord }) => {
           {displayWord.definition}
         </div>
         <div id="button-container">
-        <input
-          id="button"
-          type="button"
-          value="Next"
-          onClick={handleClick}
-        />
-        <input
-          id="button"
-          type="button"
-          value="spell"
-          onClick={spellWord}
-        />
+          <input id="button" type="button" value="Next" onClick={handleClick} />
+          <input id="button" type="button" value="spell" onClick={spellWord} />
         </div>
       </div>
     </div>
