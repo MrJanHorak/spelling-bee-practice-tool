11b9a9b73f4fdfa0303dff89e2740e8cc680933e
Jan Horak <janny_b_goode@hotmail.com>
2022-04-16 16:58:30 -0400

    update readme and add avatars

--- src/pages/Auth/AvatarSelection.jsx
+++ src/pages/Auth/AvatarSelection.jsx
@@ -11,6 +11,30 @@ import monkey from '../../assets/avatars/monkey.png'
 import panda from '../../assets/avatars/panda.png'
 import raccoon from '../../assets/avatars/raccoon.png'
 import wolf from '../../assets/avatars/wolf.png'
+import anteater from '../../assets/avatars/anteater.png'
+import bear2 from '../../assets/avatars/bear2.png'
+import buffalo from '../../assets/avatars/buffalo.png'
+import chicken from '../../assets/avatars/chicken.png'
+import cow from '../../assets/avatars/cow.png'
+import crow from '../../assets/avatars/crow.png'
+import dog from '../../assets/avatars/dog.png'
+import dog2 from '../../assets/avatars/dog2.png'
+import donkey from '../../assets/avatars/donkey.png'
+import elephant from '../../assets/avatars/elephant.png'
+import koala2 from '../../assets/avatars/koala2.png'
+import leopard from '../../assets/avatars/leopard.png'
+import octopus from '../../assets/avatars/octopus.png'
+import owl from '../../assets/avatars/owl.png'
+import panda2 from '../../assets/avatars/panda2.png'
+import parrot from '../../assets/avatars/parrot.png'
+import penguin from '../../assets/avatars/penguin.png'
+import pig from '../../assets/avatars/pig.png'
+import polarbear from '../../assets/avatars/polar-bear.png'
+import racoon from '../../assets/avatars/racoon.png'
+import rooster from '../../assets/avatars/rooster.png'
+import seagull from '../../assets/avatars/seagull.png'
+import snake from '../../assets/avatars/snake.png'
+import whale from '../../assets/avatars/whale.png'
 
 const AvatarSelection = (props) => {
 
@@ -24,14 +48,38 @@ const AvatarSelection = (props) => {
         <img src={props.formData.avatar} alt="animal-avatar"></img>
         <div className="bottom-ui">
           <select onChange={(e) => props.handleChange(e)} name="avatar" value={props.formData.avatar}>
-            <option value={cat}>Cat</option>
+            <option value={anteater}>Anteater</option>
             <option value={bear}>Bear</option>
-            <option value={monkey}>Monkey</option>
-            <option value={koala}>Koala</option>
-            <option value={fox}>Fox</option>
+            <option value={bear2}>Bear 2</option>
+            <option value={buffalo}>Buffalo</option>
+            <option value={cat}>Cat</option>
+            <option value={chicken}>Chicken</option>
+            <option value={cow}>Cow</option>
+            <option value={crow}>Crow</option>
             <option value={deer}>Deer</option>
-            <option value={raccoon}>Raccoon</option>
+            <option value={dog}>Dog</option>
+            <option value={dog2}>Dog 2</option>
+            <option value={donkey}>Donkey</option>
+            <option value={elephant}>Elephant</option>
+            <option value={fox}>Fox</option>
+            <option value={koala}>Koala</option>
+            <option value={koala2}>Koala 2</option>
+            <option value={leopard}>Leopard</option>
+            <option value={monkey}>Monkey</option>
+            <option value={octopus}>Octopus</option>
+            <option value={owl}>Owl</option>
             <option value={panda}>Panda</option>
+            <option value={panda2}>Panda 2</option>
+            <option value={parrot}>Parrot</option>
+            <option value={penguin}>Penguint</option>
+            <option value={pig}>Pig</option>
+            <option value={polarbear}>Polarbear</option>
+            <option value={raccoon}>Raccoon</option>
+            <option value={racoon}>Raccoon 2</option>
+            <option value={rooster}>Rooster</option>
+            <option value={seagull}>Seagull</option>
+            <option value={snake}>Snake</option>
+            <option value={whale}>Whale</option>
             <option value={wolf}>Wolf</option>
           </select>
           <button onClick={props.handlePopup} type="button">Confirm</button>
